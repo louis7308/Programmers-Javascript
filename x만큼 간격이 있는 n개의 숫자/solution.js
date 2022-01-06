23c259fe9f85c927acd929bffc9e534a3d0a92c0
전승원 <78971821+louis7308@users.noreply.github.com>
2022-01-06 16:59:51 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ x만큼 간격이 있는 n개의 숫자/solution.js
@@ -0,0 +1,17 @@
+// 내가 한 풀이
+function solution(x, n) {
+  let answer = [];
+  let y = x;
+  for(let i = 0; i < n; i++) {
+    answer[i] = y;
+    y += x 
+  }
+  return answer;
+}
+
+
+
+// 다른 사람 풀이
+function solution(x, n) {
+  return Array(n).fill(x).map((v, i) => (i + 1) * v)
+}
